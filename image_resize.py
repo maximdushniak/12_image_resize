140cc7897d708108ef4d633ab63b6f3f12ea6230
maximdushniak <maxim.dushniak@gmail.com>
2016-10-25 20:24:08 +0300

    начал работу с argparse

--- image_resize.py
+++ image_resize.py
@@ -1,4 +1,6 @@
+import argparse
 from PIL import Image
+
 
 def resize_image(path_to_original, path_to_result):
     pass
@@ -17,16 +19,22 @@ def get_new_size(width ,height, width_original, height_original):
 
 if __name__ == '__main__':
     
-    width_original = 1920
-    height_original = 1024
+    parser = argparse.ArgumentParser('Image resizer',description='Описание...')
 
-    if scale:
-        if width or height:
-            #ошибка
-            pass
-    elif width and height:
-        if not get_resolve_resize(width ,height,width_original, height_original):
-            pass
-    else:
-        #расчет пропорций
-        pass
+    parser.add_argument('--version', action='version', version = '1.0rc1')
+
+
+    print(parser.parse_args())
+    # width_original = 1920
+    # height_original = 1024
+
+    # if scale:
+    #     if width or height:
+    #         #ошибка
+    #         pass
+    # elif width and height:
+    #     if not get_resolve_resize(width ,height,width_original, height_original):
+    #         pass
+    # else:
+    #     #расчет пропорций
+    #     pass
