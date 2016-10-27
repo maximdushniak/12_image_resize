7132642dadc7e6ea2bc9b0fe4570c75cff081e2f
maximdushniak <maxim.dushniak@gmail.com>
2016-10-27 19:56:51 +0300

    закончил с argparse

--- image_resize.py
+++ image_resize.py
@@ -19,79 +19,61 @@ def get_new_size(width ,height, width_original, height_original):
     pass
 
 
-   
+def get_output_filename(output, original_image, width, height):
+    if output:
+        return output
+    else:
+        list_filepath = os.path.splitext(original_image.name)
+        file_name = list_filepath[0]
+        file_ext = list_filepath[1]
+        return file_name + '_' + str(width) + 'x' + str(height) + file_ext
 
 
 def create_parser():
     parser = argparse.ArgumentParser(prog='Image resizer')
 
-    # parser.add_argument('file', nargs='?', type=argparse.FileType())
-    # parser.add_argument('-w', '--width', type=int)
-    # parser.add_argument('-H', '--height', type=int)
-    # parser.add_argument('-s', '--scale', type=float)
-    # parser.add_argument('-o', '--output', type=argparse.FileType(mode='w'))
+    parser.add_argument('file', nargs=1, type=Image.open)
 
+    parser.add_argument('-s', '--scale', nargs='?', type=float)
 
-    subparsers = parser.add_subparsers(dest='command')
+    parser.add_argument('-w', '--width', nargs='?', type=int)
+    parser.add_argument('-H', '--height', nargs='?', type=int)
 
-    scale_parser = subparsers.add_parser('scale')
-    scale_parser.add_argument('file', nargs='+', type=Image.open)
-    # scale_parser.add_argument('-s', '--scale', nargs='?', type=float)
-    scale_parser.add_argument('-o', '--output', type=argparse.FileType(mode='w'))
-
-    size_parser = subparsers.add_parser('size')
-    size_parser.add_argument('file', nargs='+', type=argparse.FileType())
-    size_parser.add_argument('-w', '--width', nargs='?', type=int)
-    size_parser.add_argument('-H', '--height', nargs='?', type=int)
-    size_parser.add_argument('-o', '--output', type=argparse.FileType(mode='w'))
+    parser.add_argument('-o', '--output', nargs='?',type=Image.open)
 
     return parser
-
 
 
 if __name__ == '__main__':
 
     parser = create_parser()
-    namespace = parser.parse_args(sys.argv[1:])
-    
-    # if namespace.scale:
-    #     if namespace.width or namespace.height:
+
+    # namespace = parser.parse_args(sys.argv[1:])                #!!!TODO del comment
+    namespace = parser.parse_args(['blabla.bla.jpg', '-s', '2'])
+
+    image_file = namespace.file[0]
+
+    original_width = image_file.width
+    original_height = image_file.height
+    original_resolution = original_width/original_height
+
+    if namespace.scale:
+        if namespace.width or namespace.height:
+            print('Error')
+            print(parser.print_help())
+            exit(1)
+        width = namespace.scale*original_width
+        height = namespace.scale*original_height
+    else:
+        width = namespace.width if namespace.width else original_width*namespace.height/original_height
+        height = namespace.height if namespace.height else original_height*namespace.width/original_width
+
+    width = int(width)
+    height = int(height)
+
+    if get_resolve_resize(width, height, original_width, original_height):
+        output = get_output_filename(namespace.output, image_file, width, height)
 
 
-    # width = 100
-    # height = 200
-
-    # list_filepath = os.path.splitext(namespace.file.name)
-    # file_name = list_filepath[0]
-    # file_ext = list_filepath[1]
-
-    # if namespace.output:
-    #     resultpath = namespace
-    # else:
-    #     resultpath = file_name + '_' + str(width) + 'x' + str(height) + file_ext
-
-    # print()
-    # print(resultpath)
-
-
-    print()
-    print(namespace)
-
-    # if namespace.filepath:
-    #     print ("Привет, {}!".format (namespace.filepath) )
-    # else:
-    #     print ("Привет, мир!")
-
-    # width_original = 1920
-    # height_original = 1024
-
-    # if scale:
-    #     if width or height:
-    #         #ошибка
-    #         pass
-    # elif width and height:
-    #     if not get_resolve_resize(width ,height,width_original, height_original):
-    #         pass
-    # else:
-    #     #расчет пропорций
-    #     pass
+    print(original_width, original_height)
+    print(width, height)
